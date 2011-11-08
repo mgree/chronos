4eef03604a907ffb78c21e36dff54a831c1089e1
Michael Greenberg <mgree@seas.upenn.edu>
2011-11-08 11:32:10 -0500

    Tabbed view, renamed.  (Sort of.  Eclipse is silly.)

---
+++ Stopwatch/src/com/weaselhat/chronos/ChronosTabActivity.java
@@ -0,0 +1,39 @@
+package com.weaselhat.chronos;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class ChronosTabActivity extends TabActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		TabHost tabHost = getTabHost();  // The activity TabHost
+		TabHost.TabSpec spec;  // Reusable TabSpec for each tab
+		Intent intent;  // Reusable Intent for each tab
+
+		// Initialize intents and tab specs
+		intent = new Intent().setClass(this, TimerActivity.class);
+		spec = tabHost.newTabSpec("timer")
+			    .setIndicator("Timer")
+				.setContent(intent);
+		tabHost.addTab(spec);
+
+		intent = new Intent().setClass(this, StopwatchActivity.class);
+		spec = tabHost.newTabSpec("stopwatch")
+			    .setIndicator("Stopwatch")
+				.setContent(intent);
+		tabHost.addTab(spec);
+
+		intent = new Intent().setClass(this, AlarmActivity.class);
+		spec = tabHost.newTabSpec("alarm")
+				.setIndicator("Alarm")
+				.setContent(intent);
+		tabHost.addTab(spec);
+
+		tabHost.setCurrentTab(2);
+	}
+}
--- Stopwatch/src/com/weaselhat/chronos/StopwatchActivity.java
+++ Stopwatch/src/com/weaselhat/chronos/StopwatchActivity.java
@@ -1,13 +1,16 @@
-package com.weaselhat.stopwatch;
+package com.weaselhat.chronos;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StopwatchActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        TextView textview = new TextView(this);
+        textview.setText("This is the stopwatch tab");
+        setContentView(textview);
     }
 }
